78a90376eec4ce442d68e387aa0172635d0a6df0
m.s.bekbaev <m.s.bekbaev@gmail.com>
2021-02-12 19:33:44 +0300

    inition commit

---
+++ script.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+import pigpio
+
+
+#GPIO pin 12 = BCM pin 18 = wiringpi pin 1 
+led_pin = 18
+
+print("Don't forget to start with sudo pigpiod")
+
+pi = pigpio.pi()
+
+faceCascade = cv2.CascadeClassifier('haar.xml')
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+
+    pi.set_PWM_dutycycle(led_pin, len(faces) * 16)
+    pi.set_PWM_frequency(4,600)
+    pi.set_PWM_dutycycle(4, 128 if len(faces) > 0 else 0)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+
+    cv2.imshow('video',img)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+
+pi.set_PWM_dutycycle(led_pin, 0)
+pi.set_PWM_dutycycle(4, 0)
+cap.release()
+cv2.destroyAllWindows()
